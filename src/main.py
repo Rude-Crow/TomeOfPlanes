ee867cb17c024c88924eaab4608b5c36e51c611c
Jamie Stead <jamie.p.r.stead@gmail.com>
2025-08-08 21:03:26 +0100

    wrapped planesmanager in screen

--- src/main.py
+++ src/main.py
@@ -5,11 +5,16 @@ from ui.planes_manager_page import PlanesManager
 class MainScreen(Screen):
     pass
 
+class PlanesScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.add_widget(PlanesManager())
+
 class TomeApp(App):
     def build(self):
         sm = ScreenManager()
-        sm.add_widget(MainScreen(name="main"))
-        sm.add_widget(PlanesManager(name="planes"))
+        sm.add_widget(MainScreen(name = 'main'))
+        sm.add_widget(PlanesScreen(name = 'planes_manager'))
         return sm
 
 if __name__ == '__main__':

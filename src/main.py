414ba2a694c2f595fbd2d1b029bc4cf21ad5c1fe
Jamie Stead <jamie.p.r.stead@gmail.com>
2025-08-08 23:41:19 +0100

    fixed basic ui style

--- src/main.py
+++ src/main.py
@@ -1,21 +1,18 @@
 from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager, Screen
-from ui.planes_manager_page import PlanesManager
+from kivy.uix.boxlayout import BoxLayout
+from kivy.lang import Builder
 
-class MainScreen(Screen):
+Builder.load_file("tome_style.kv")
+
+
+class OpenBookLayout(BoxLayout):
     pass
 
-class PlanesScreen(Screen):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.add_widget(PlanesManager())
 
-class TomeApp(App):
+class TomeOfPlanes(App):
     def build(self):
-        sm = ScreenManager()
-        sm.add_widget(MainScreen(name = 'main'))
-        sm.add_widget(PlanesScreen(name = 'planes_manager'))
-        return sm
+        return OpenBookLayout()
 
-if __name__ == '__main__':
-    TomeApp().run()
+
+if __name__ == "__main__":
+    TomeOfPlanes().run()
